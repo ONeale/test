72d493cebd8c68ce719517ad22d77a81440344f6
Olivia <mail@oliviabeale.com>
2014-07-08 16:49:10 +0100

    edot

--- web.js
+++ web.js
@@ -1,18 +1,15 @@
-var http = require('http'); //how we require modules
+// web.js
+var express = require("express");
+var logfmt = require("logfmt");
+var app = express();
 
-http.createServer(function (request, response){
-	response.writeHead(200); //status code in header
-	response.write("Hello, World"); //response body
+app.use(logfmt.requestLogger());
 
-	setTimeout(function() {
-		response.write('Dog is done.');
-		response.end();
-	}, 5000); //5000 milliseconds = 5 secs
-	response.end(); //close the connection
-}).listen(8080); //listen for connections on this port
-console.log('listening on port 8080')
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
 
-//register request event
-//then event loop continuosly. run callback and prints hello this is dog
-//js is evented programming in event loops.
-//no blocking code
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
